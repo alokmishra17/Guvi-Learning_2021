b3597434a4a46b9a58b8106a39140dd483259580
alokmishra17 <akmishra1717@gmail.com>
2021-07-23 07:01:21 +0530

    day 4 task complete

---
+++ Assignment-taskB261/day-4/script1.js
@@ -0,0 +1,50 @@
+let myData ={
+    info: {
+      name: "Alok Mishra",
+      age: "23",
+      gender: "Male",
+      dob: "17-02-1998"
+    },
+    address:{
+      country: "India",
+      state: "Himachal Pradesh",
+      district: "Sirmaur",
+      city: "Paonta Sahib",
+      pin: 173025
+    },
+    education:{
+      matriculation:{
+        year: 2014,
+        score: "93%",
+        board: "HP State Board",
+        place: "Himachal Pradesh"
+      },
+      secondary:{
+        year: 2016,
+        score: "75%",
+        board: "CBSE",
+        place: "Himachal Pradesh"
+      },
+      graduation:{
+        year: 2021,
+        marks: "78%",
+        university: "HPTU",
+        place: "Himachal Pradesh"
+      }
+    },
+    skills:{
+      programming: "C/C++, Python, Java, JavaScript",
+      softskills: "Adaptibility, Quick Learner, strong work ethics ",
+      techskills: "AWS, Git, Linux"
+      },
+    experience:{
+      work: "fresher",
+      certification:"Python, AWS Cloud, Django-framework"
+    },
+    others:{
+      interest: "Cricket, Wathing Movies, Political Affairs",
+      aim: "Become Software Developer"
+    }
+  }
+    
+  console.log(myData);
